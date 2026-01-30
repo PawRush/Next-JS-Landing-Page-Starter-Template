407785973ea5b7a91672cdd43c18b7f09f242106
Sergey Kazantsev <sergeyka@amazon.com>
2026-01-30 03:33:25 +0100

    chore: initialize CDK infrastructure foundation

---
+++ infra/bin/infra.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+
+// Temporary placeholder - will be replaced in Step 7
+const app = new cdk.App();
+
+// eslint-disable-next-line no-new
+new cdk.Stack(app, 'PlaceholderStack');
